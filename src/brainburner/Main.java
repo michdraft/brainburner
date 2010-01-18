22a8e1c800cb4f171ee37ac96d02d3e648daf2eb
Lukas Grässlin <lukasgraesslin@gmx.de>
2010-01-18 18:56:17 +0100

    Change formatting optiones for the project and reindent the code
    * Don't expand tabs to spaces
    * Use 8 space-tabs

--- src/brainburner/Main.java
+++ src/brainburner/Main.java
@@ -2,13 +2,12 @@
  * Main.java, package: brainburner
  * Main class of the project. Won't do any magic.
  */
-
 package brainburner;
 
 import gui.Frame;
 
 public class Main {
-    public static void main(String[] args) {
-        new Frame();
-    }
+	public static void main(String[] args) {
+		new Frame();
+	}
 }
--- src/gui/Frame.java
+++ src/gui/Frame.java
@@ -2,7 +2,6 @@
  * Frame.java, package: gui
  * Extends JFrame and offers the base class for our frames.
  */
-
 package gui;
 
 import java.awt.Dimension;
@@ -12,19 +11,19 @@ import javax.swing.UIManager;
 import helper.*;
 
 public class Frame extends JFrame {
-    public Frame() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setMinimumSize(new Dimension(500, 350));
-        this.setVisible(true);
+	public Frame() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setMinimumSize(new Dimension(500, 350));
+		this.setVisible(true);
 
-        /*
-         * Get the system style und try to use it for the frames.
-         */
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            Messages.showWarning("Error while setting LookAndFeel:\n"
-                    + e.getMessage());
-        }
-    }
+		/*
+		 * Get the system style und try to use it for the frames.
+		 */
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			Messages.showWarning("Error while setting LookAndFeel:\n"
+				+ e.getMessage());
+		}
+	}
 }
