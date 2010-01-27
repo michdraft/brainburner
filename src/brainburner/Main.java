afaa2b7a43e9823230eaba4889951f184d12a0b5
Lukas Grässlin <lukasgraesslin@gmx.de>
2010-01-27 11:58:28 +0100

    Some diagramm updates etc

--- src/brainburner/Main.java
+++ src/brainburner/Main.java
@@ -11,13 +11,13 @@ import gui.*;
 public class Main {
 	public static void main(String[] args) {
 
-		String db_name = "test_db";
+		String db_name = "brainburner_test";
 		String db_user = "test";
 		String db_password = "test";
 
 		DBConnection connection = new DBConnection(db_name, db_user, db_password);
 
-		//new Users(connection); /* just for testing */
-		new TablesFrame(connection); /* just for testing */
+		new UsersFrame(connection); /* just for testing */
+		//new TablesFrame(connection); /* just for testing */
 	}
 }
