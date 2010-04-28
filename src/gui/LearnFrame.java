58539517368682690246bc30fbd01030ada4a555
Lukas Grässlin <lukasgraesslin@gmx.de>
2010-04-28 18:28:16 +0200

    implemented re-learning

--- src/gui/LearnFrame.java
+++ src/gui/LearnFrame.java
@@ -8,11 +8,14 @@ import data.objects.User;
 import helper.Helpers;
 import helper.Messages;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import javax.swing.JOptionPane;
 
 public class LearnFrame extends Frame {
 
 	private ArrayList<String[]> datasets;
+	private ArrayList<String[]> wrong_datasets;
 	private int idx;
 	private User user;
 	private ExerciseArea exerciseareaid;
@@ -32,6 +35,7 @@ public class LearnFrame extends Frame {
 		this.connection = connection;
 		this.datasets = datasets;
 		this.idx = 0;
+		this.wrong_datasets = new ArrayList<String[]>();
 
 		this.getRootPane().setDefaultButton(jButton1);
 		this.askQuestion();
@@ -39,73 +43,73 @@ public class LearnFrame extends Frame {
 
 
 	@SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
 
-        lbl_headline = new javax.swing.JLabel();
-        lbl_question = new javax.swing.JLabel();
-        txt_question = new javax.swing.JTextField();
-        lbl_answer = new javax.swing.JLabel();
-        txt_answer = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+                lbl_headline = new javax.swing.JLabel();
+                lbl_question = new javax.swing.JLabel();
+                txt_question = new javax.swing.JTextField();
+                lbl_answer = new javax.swing.JLabel();
+                txt_answer = new javax.swing.JTextField();
+                jButton1 = new javax.swing.JButton();
 
-        lbl_headline.setFont(new java.awt.Font("Dialog", 1, 18));
-        lbl_headline.setText("Trainer");
+                lbl_headline.setFont(new java.awt.Font("Dialog", 1, 18));
+                lbl_headline.setText("Trainer");
 
-        lbl_question.setText("Question:");
+                lbl_question.setText("Question:");
 
-        txt_question.setDisabledTextColor(new java.awt.Color(0, 0, 0));
-        txt_question.setEnabled(false);
+                txt_question.setDisabledTextColor(new java.awt.Color(0, 0, 0));
+                txt_question.setEnabled(false);
 
-        lbl_answer.setText("Answer:");
+                lbl_answer.setText("Answer:");
 
-        jButton1.setText("confirm");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+                jButton1.setText("Confirm");
+                jButton1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jButton1ActionPerformed(evt);
+                        }
+                });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(lbl_headline)
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(lbl_answer, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(lbl_question, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(txt_question)
-                                .addComponent(txt_answer, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbl_headline)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_question)
-                    .addComponent(txt_question, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_answer)
-                    .addComponent(txt_answer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(jButton1)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                .addComponent(lbl_headline)
+                                                .addGroup(layout.createSequentialGroup()
+                                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                                                .addComponent(lbl_answer, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(lbl_question, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                                                .addComponent(txt_question)
+                                                                .addComponent(txt_answer, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)))))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(lbl_headline)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(lbl_question)
+                                        .addComponent(txt_question, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(lbl_answer)
+                                        .addComponent(txt_answer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jButton1)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+                pack();
+        }// </editor-fold>//GEN-END:initComponents
 
 	private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 		checkAnswer(this.txt_answer.getText().trim());
@@ -141,13 +145,40 @@ public class LearnFrame extends Frame {
 			this.words_right++;
 		} else {
 			Messages.showInfo("wrong answer");
+			this.wrong_datasets.add(datasets.get(idx));
 			this.datasets.remove(idx);
 			this.words_wrong++;
 		}
 
 		/* Learning is finished */
-		if (this.datasets.isEmpty())
-			saveStatistic();
+		if (this.datasets.isEmpty()) {
+			if (this.wrong_datasets.isEmpty()) {
+				saveStatistic();
+			} else {
+				Object[] options = { "Yes", "No" };
+				int res =
+					JOptionPane.showOptionDialog(null,
+						"You answered " + wrong_datasets.size() + " words wrong. Do you want to learn these again?",
+						"Learn again",
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null, options, null);
+
+				if (res == 0)
+					learnAgain();
+				else
+					saveStatistic();
+			}
+		}
+	}
+
+	private void learnAgain() {
+		for (String[] s : wrong_datasets) {
+			this.datasets.add(s);
+		}
+
+		this.wrong_datasets.removeAll(wrong_datasets);
+		askQuestion();
 	}
 
 	private void saveStatistic() {
@@ -157,13 +188,13 @@ public class LearnFrame extends Frame {
 		Statistics.addStatistic(connection, s);
 	}
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel lbl_answer;
-    private javax.swing.JLabel lbl_headline;
-    private javax.swing.JLabel lbl_question;
-    private javax.swing.JTextField txt_answer;
-    private javax.swing.JTextField txt_question;
-    // End of variables declaration//GEN-END:variables
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JButton jButton1;
+        private javax.swing.JLabel lbl_answer;
+        private javax.swing.JLabel lbl_headline;
+        private javax.swing.JLabel lbl_question;
+        private javax.swing.JTextField txt_answer;
+        private javax.swing.JTextField txt_question;
+        // End of variables declaration//GEN-END:variables
 
 }
--- src/gui/TrainExerciseAreaFrame.java
+++ src/gui/TrainExerciseAreaFrame.java
@@ -29,73 +29,71 @@ public class TrainExerciseAreaFrame extends Frame {
 	}
 
 	@SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
 
-        lbl_header = new javax.swing.JLabel();
-        cb_learn_table = new javax.swing.JComboBox();
-        lbl_learn_table = new javax.swing.JLabel();
-        btn_train = new javax.swing.JButton();
-        btn_cancel = new javax.swing.JButton();
+                lbl_header = new javax.swing.JLabel();
+                cb_learn_table = new javax.swing.JComboBox();
+                lbl_learn_table = new javax.swing.JLabel();
+                btn_train = new javax.swing.JButton();
+                btn_cancel = new javax.swing.JButton();
 
-	  setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);						
+                lbl_header.setFont(new java.awt.Font("Dialog", 1, 18));
+                lbl_header.setText("Train Learn table");
 
-        lbl_header.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        lbl_header.setText("Train Learn table");
+                lbl_learn_table.setText("Learn Table:");
 
-        lbl_learn_table.setText("Learn Table:");
+                btn_train.setText("Train");
+                btn_train.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn_trainActionPerformed(evt);
+                        }
+                });
 
-        btn_train.setText("train");
-        btn_train.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_trainActionPerformed(evt);
-            }
-        });
+                btn_cancel.setText("Cancel");
+                btn_cancel.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                btn_cancelActionPerformed(evt);
+                        }
+                });
 
-        btn_cancel.setText("Cancel");
-        btn_cancel.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_cancelActionPerformed(evt);
-            }
-        });
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lbl_header)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(lbl_learn_table)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(cb_learn_table, 0, 150, Short.MAX_VALUE)
+                                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                                .addComponent(btn_train)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(btn_cancel)))))
+                                .addContainerGap())
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(lbl_header)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(cb_learn_table, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lbl_learn_table))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(btn_train)
+                                        .addComponent(btn_cancel))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_header)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lbl_learn_table)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(cb_learn_table, 0, 150, Short.MAX_VALUE)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(btn_train)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btn_cancel)))))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbl_header)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cb_learn_table, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl_learn_table))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btn_train)
-                    .addComponent(btn_cancel))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+                pack();
+        }// </editor-fold>//GEN-END:initComponents
 
 	private void btn_trainActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_trainActionPerformed
 		this.toggleVisibility(); this.createArrayList();
@@ -173,11 +171,11 @@ public class TrainExerciseAreaFrame extends Frame {
 		}
 	}
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_cancel;
-    private javax.swing.JButton btn_train;
-    private javax.swing.JComboBox cb_learn_table;
-    private javax.swing.JLabel lbl_header;
-    private javax.swing.JLabel lbl_learn_table;
-    // End of variables declaration//GEN-END:variables
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JButton btn_cancel;
+        private javax.swing.JButton btn_train;
+        private javax.swing.JComboBox cb_learn_table;
+        private javax.swing.JLabel lbl_header;
+        private javax.swing.JLabel lbl_learn_table;
+        // End of variables declaration//GEN-END:variables
 }
